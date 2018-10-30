09871bf8205ec4810613bec0b1f4ec7c06806198
Tom Kosters <tom.kosters@gmail.com>
2018-10-31 00:36:16 +0100

    Made 63

---
+++ week7/ex63/63.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    double value = 12.04;
+
+    cout << setw(15) << value << '\n'
+         << setw(15) << left << value << '\n'
+         << setw(15) << right << value << '\n'
+         << fixed
+         << setw(15) << setprecision(1) << value << '\n'
+         << setw(15) << setprecision(4) << value << '\n'
+         << defaultfloat
+         << setw(15) << value << '\n';
+}
