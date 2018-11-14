890df1bd774c74fdde65fd0438a680be53f80bfb
Tom Kosters <tom.kosters@gmail.com>
2018-11-14 22:44:01 +0100

    Changed ex58

--- week7/ex58/main.cc
+++ week7/ex58/main.cc
@@ -5,5 +5,8 @@ int main(int argc, char **argv)
     cout.setstate(ios::failbit);
 
     for (size_t idx = 0; idx != stoul(argv[1]); ++idx)
-        cout << "Nr. of command line arguments " << argc << '\n';
+    {
+    	if (cout.good())
+        	cout << "Nr. of command line arguments " << argc << '\n';
+    }
 }
