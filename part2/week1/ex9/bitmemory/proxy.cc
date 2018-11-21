1b9a9ebc4d0532e2193bb45ca7071fb8f1072267
Jos van Goor <josvangoor@gmail.com>
2018-11-21 17:56:58 +0100

    offset in proxy.cc

--- part2/week1/ex9/bitmemory/proxy.cc
+++ part2/week1/ex9/bitmemory/proxy.cc
@@ -2,6 +2,7 @@
 
 BitMemory::Proxy BitMemory::proxy(size_t idx) const
 {
+    idx += capacity() - max_bit_nr();
     return BitMemory::Proxy(
         d_bits[idx / blocksize()],
         idx % blocksize()
