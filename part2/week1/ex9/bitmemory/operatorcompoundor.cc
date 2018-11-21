17e6a196da8d7a6d060b2df7f593ebbb7253b1d2
Jos van Goor <josvangoor@gmail.com>
2018-11-21 17:26:41 +0100

    or

--- part2/week1/ex9/bitmemory/operatorcompoundor.cc
+++ part2/week1/ex9/bitmemory/operatorcompoundor.cc
@@ -3,12 +3,12 @@
 BitMemory &BitMemory::operator|=(BitMemory const &mem)
 {
     if (mem.d_nbits > d_nbits)
-        enlarge(d_nbits + mem.d_nbits);
+        enlarge(mem.d_nbits);
 
-    size_t difference = d_nbits - mem.d_nbits;
-
-    for (size_t idx = difference; idx < (d_nbits / blocksize()); ++idx)
-        d_bits[idx] |= mem.d_bits[idx - difference];
+    for (size_t idx = 0; idx < mem.d_nbits; ++idx)
+    {
+        (*this)[max_bit_nr() - 1 - idx] |= mem[mem.max_bit_nr() - 1 - idx];
+    }
 
     return *this;
 }
