7e8d1900cb1bbf7489d49dfcb685bba3c0e7ef75
Tom Kosters <tom.kosters@gmail.com>
2018-12-04 12:15:30 +0100

    fixed 16

--- part2/week2/ex16/ex16.cc
+++ part2/week2/ex16/ex16.cc
@@ -5,77 +5,43 @@ using namespace std;
 
 class ExitLeaks
 {
-    string **sentences;
-
     public:
         ExitLeaks();
         ~ExitLeaks();
-        void fun();
 };
 
 ExitLeaks::ExitLeaks()
 {
     cout << "constructor called!\n";
-    sentences = new string *[20];
-    for (size_t idx = 0; idx != 20; ++idx)
-    {
-        sentences[idx] = new string[20];
-    }
 }
 
 ExitLeaks::~ExitLeaks()
 {
     cout << "destructor called!\n";
-    for (size_t idx = 0; idx != 20; ++idx)
-    {
-        delete[] sentences[idx];
-    }
-    delete[] sentences;
-}
-
-// useless function to ensure object is not removed as unused
-void ExitLeaks::fun()
-{
-    cout << "...\n";
-}
-
-// this function throws an exception, and the local object is destroyed
-void wellBehavedFunc()
-{
-    ExitLeaks well_behaved{};
-    well_behaved.fun();
-    throw string("something terrible happened! (but it's handled well)\n");
 }
 
 // this function exits, and the object is not destroyed
 void leakyFunc()
 {
-    ExitLeaks lil_leaky{};
-    lil_leaky.fun();
+    ExitLeaks leaky{};
     exit(1);
+}
+
+// this function throws an exception instead; the object is properly destroyed
+void exceptionFunc()
+{
+    ExitLeaks not_leaky{};
+    throw string("exception message goes here\n");
 }
 
 int main()
 {
-    cout << "[1] Well-behaved Function\n[2] Leaky Function\n> ";
-    int choice;
-    cin >> choice;
-
-    // well behaved function is called from within a try block, after the
-    // exception is thrown the destructor of the local object is called
-    if (choice == 1)
+    try
     {
-        try
-        {
-            wellBehavedFunc();
-        }
-        catch (string str)
-        {
-            cout << str;
-            return 1;
-        }
+        exceptionFunc();
     }
-    
-    if (choice == 2)
-        leakyFunc();
+    catch(string str)
+    {
+        cout << str;
+    }
 }
