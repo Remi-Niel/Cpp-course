a2c58cafcf13dbdd9755e62c36e7090c1bcc9631
Jos van Goor <josvangoor@gmail.com>
2018-12-11 15:21:07 +0100

    fixxed 22 and 23

--- part2/week3/ex22/extstring.h
+++ part2/week3/ex22/extstring.h
@@ -3,14 +3,11 @@
 class ExtString: public std::string
 {
     public:
-        using std::string::string;
         ExtString(size_t count, std::string const &str);
 };
 
 ExtString::ExtString(size_t count, std::string const &str)
 {
     for (; count != 0; --count)
-    {
-        this->append(str);
-    }
+        append(str);
 }
