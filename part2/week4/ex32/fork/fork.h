a88b042da111bf1c46b626938073a66a169c1fde
Jos van Goor <josvangoor@gmail.com>
2018-12-12 14:07:01 +0100

    ex32

---
+++ part2/week4/ex32/fork/fork.h
@@ -0,0 +1,25 @@
+#ifndef INCLUDED_FORK_
+#define INCLUDED_FORK_
+
+#include <sys/types.h>
+
+class Fork
+{
+    pid_t d_pid = 0;
+
+    public:
+        void fork();
+
+        virtual void childProcess() = 0;
+        virtual void parentProcess() = 0;
+    
+    protected:
+        Fork() = default;
+        Fork(Fork const& copy) = delete; //no copy or move
+        Fork &operator=(Fork const& copy) = delete; //no copy or move
+
+        pid_t pid() const;
+        int waitForChild() const;
+};
+        
+#endif
