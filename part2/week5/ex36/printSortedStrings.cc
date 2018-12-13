a8c19af1ff16ac0bed6d137338e25db14f130e86
Tom Kosters <tom.kosters@gmail.com>
2018-12-13 13:52:18 +0100

    Formatting, comments

--- part2/week5/ex36/printSortedStrings.cc
+++ part2/week5/ex36/printSortedStrings.cc
@@ -9,9 +9,11 @@ int main()
 	priority_queue<string> string_queue;
 	string input;
 
+	// grab all strings fropm input and push them onto the priority queue
 	while (cin >> input)
 		string_queue.push(input);
 
+	// print the top string, pop it off, repeat
 	while (string_queue.size())
 	{
 		cout << string_queue.top() << '\n';
--- part2/week5/ex37/printStringsAndCounts.cc
+++ part2/week5/ex37/printStringsAndCounts.cc
@@ -11,10 +11,13 @@ int main()
 	string input;
 	while (cin >> input)
 	{
-			pair<map<string, size_t>::iterator, bool> ret;
-			ret = wordcountmap.insert(pair<string, size_t>(input, 1));
-			if (ret.second == false)
-				wordcountmap[input]++;
+		// try to insert the string into the map (as a key)
+		pair<map<string, size_t>::iterator, bool> ret;
+		ret = wordcountmap.insert(pair<string, size_t>(input, 1));
+
+		// if string already present, increase count
+		if (ret.second == false)
+			wordcountmap[input]++;
 	}
 
 	for (map<string,size_t>::iterator iter = wordcountmap.begin();
