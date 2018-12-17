61f0db3a949079e547f36862181dd4a69fd48c77
Tom Kosters <tom.kosters@gmail.com>
2018-12-17 16:02:17 +0100

    signal interface done, handler WIP

--- part2/week5/ex42/signalclass.h
+++ part2/week5/ex42/signalclass.h
@@ -7,23 +7,24 @@ class Signal
 {
     class Handler;
 
+    std::multimap<size_t, Handler> d_signal_map;
+
     public:
         static Signal *accessSignal();
         void signal(int signum, sighandler_t handler);
+
+        // functions that handle Handler objects
+        void add(size_t signum, Handler &object);
+        void remove(size_t signum, Handler &object);
+        void ignore(size_t signum);
+        void reset(size_t signum);
         
         // delete copy and move constructors (Singleton)
         Signal(Signal &other) = delete;
         Signal(Signal &&tmp) = delete;
 
-        // test stuff
-        void printStuff()
-        {
-            std::cout << "stuff\n";
-        }
-
     private:
         Signal() = default;
-
 };
 
 inline Signal *Signal::accessSignal()
