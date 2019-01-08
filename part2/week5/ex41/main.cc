da7860cd3e2e3451fa6ea2de90a938fdc2b83cbb
ewf@fdv.as <ewf@fdv.as>
2019-01-08 19:34:39 +0100

    correct 41, left old solution as a comment

--- part2/week5/ex41/main.cc
+++ part2/week5/ex41/main.cc
@@ -11,24 +11,30 @@ int main(int argc, char **argv)
 {
     unordered_multimap<string, string> container;
     
-    // fill the container with data (no need to implement this)
 
-    // this is probably not the requested answer, but it's something...
-    size_t nUniqueKeys = count_if
-    (
-        container.begin(),
-        container.end(),
-        bind
-        (
-            [](pair<string, string> const &entry, unordered_set<string> &keys)
-            {
-                auto result = keys.insert(entry.first);
-                return result.second;
-            },
-            placeholders::_1,
-            unordered_set<string>()
-        )
-    );
+    size_t nUniqueKeys = 0;
+
+    //For each unique element that occurs in the set,
+    // add 1/n n times to the count. Where n is the
+    // amount of times it occurs in the container
+    for (pair<string, string> &entry, container)
+    	nUniqueKeys += 1 / container.count(entry);
+
+    // size_t nUniqueKeys = count_if
+    // (
+    //     container.begin(),
+    //     container.end(),
+    //     bind
+    //     (
+    //         [](pair<string, string> const &entry, unordered_set<string> &keys)
+    //         {
+    //             auto result = keys.insert(entry.first);
+    //             return result.second;
+    //         },
+    //         placeholders::_1,
+    //         unordered_set<string>()
+    //     )
+    // );
     
     cout << "There are " << nUniqueKeys << " in the container\n";
 }
