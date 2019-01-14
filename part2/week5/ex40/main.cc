9cc9870d7b1faf6568587ff4b52d13c398358e8e
Jos van Goor <josvangoor@gmail.com>
2019-01-14 16:25:23 +0100

    some changes to 40

--- part2/week5/ex40/main.cc
+++ part2/week5/ex40/main.cc
@@ -1,11 +1,4 @@
-#include <vector>
-#include <iostream>
-#include <set>
-#include <string>
-#include <algorithm>
-#include "vectorwrapper/vectorwrapper.h"
-
-using namespace std;
+#include "main.ih"
 
 vector<string> read_input()
 {
@@ -21,21 +14,27 @@ vector<string> read_input()
 int main()
 {
 	vector<string> strings = read_input();
-    cout << "Size: " << strings.size() << " , capacity: " << strings.capacity() << '\n';
+    cout << "Size: " << strings.size() << " , capacity: "
+        << strings.capacity() << '\n';
 
     strings.push_back(string("extra"));
-    cout << "Size: " << strings.size() << " , capacity: " << strings.capacity() << '\n';
+    cout << "Size: " << strings.size() << " , capacity: "
+        << strings.capacity() << '\n';
     
     vector<string>(strings).swap(strings); // Sheds excess capacity
-    cout << "Size: " << strings.size() << " , capacity: " << strings.capacity() << "\n\n";
+    cout << "Size: " << strings.size() << " , capacity: " 
+        << strings.capacity() << "\n\n";
 
 
     VectorWrapper wrapper;
-    cout << "Size: " << wrapper.size() << " , capacity: " << wrapper.capacity() << '\n';
+    cout << "Size: " << wrapper.size() << " , capacity: " 
+        << wrapper.capacity() << '\n';
 
     wrapper += string("extra");
-    cout << "Size: " << wrapper.size() << " , capacity: " << wrapper.capacity() << '\n';
+    cout << "Size: " << wrapper.size() << " , capacity: " 
+        << wrapper.capacity() << '\n';
 
     VectorWrapper(wrapper).swap(wrapper); // Sheds excess capacity
-    cout << "Size: " << wrapper.size() << " , capacity: " << wrapper.capacity() << '\n';
+    cout << "Size: " << wrapper.size() << " , capacity: "
+        << wrapper.capacity() << '\n';
 }
