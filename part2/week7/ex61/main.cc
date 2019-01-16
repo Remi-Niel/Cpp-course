ce8eb8655f9f28769b20eb3bf063dc5c48eae6b1
Jos van Goor <josvangoor@gmail.com>
2019-01-16 13:50:59 +0100

    ex61

---
+++ part2/week7/ex61/main.cc
@@ -0,0 +1,26 @@
+#include "main.ih"
+
+recursive_mutex TSosWrap::s_lock{};
+
+void f1()
+{
+    TSosWrap{ std::cout } << "one " << "two " << "three ";
+}
+    
+void f2()
+{
+    TSosWrap tout{ std::cout };
+    tout << "four "  << "five " << "six ";
+    f1();
+    tout << '\n';
+}
+
+int main()
+{
+    vector<thread> threads;
+    for (size_t ix = 0; ix != 100; ++ix)
+        threads.push_back(thread(f2));
+
+    for (auto &thread: threads)
+        thread.join();
+}
