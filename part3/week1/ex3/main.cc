05ec252adaa10473d2397721544e4d3b5cddfd97
Jos van Goor <josvangoor@gmail.com>
2019-02-19 20:40:48 +0100

    improved ex3

--- part3/week1/ex3/main.cc
+++ part3/week1/ex3/main.cc
@@ -6,18 +6,5 @@ using namespace std;
 template <typename T>
 T* rawCapacity(size_t size)
 {
-    return new T[size];
-}
-
-
-int main()
-{
-    size_t length = 10;
-    string *strings = rawCapacity<string>(length);
-
-    for (size_t idx = 0; idx != length; ++idx)
-        strings[idx] = "test";
-
-    for (size_t idx = 0; idx != length; ++idx)
-        cout << strings[idx] << idx << '\n';
+    return reinterpret_cast<T *>(new char[size * sizeof(T)]);
 }
