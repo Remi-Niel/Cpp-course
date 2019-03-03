26328106ddbe62597431329e49eb9fe94afa1f85
Remi <r.f.niel@student.rug.nl>
2019-03-03 15:06:18 +0100

    corrected merge

--- part3/week3/ex24/main.cc
+++ part3/week3/ex24/main.cc
@@ -7,14 +7,8 @@ using namespace std;
 
 template<size_t val,char... chars>
 struct I2C_Helper{
-<<<<<<< HEAD
-    // add the last number of val to the front of the parameter pack,
-    // and recursively call I2C_Helper::res with val = val/10
-    static constexpr char const* res= I2C_Helper<val/10, '0' + val % 10, chars...>::res;
-=======
     static constexpr char const* res =
         I2C_Helper<val/10, '0' + val % 10, chars...>::res;
->>>>>>> 90d8ce7b18557039cc30bd6f0306e2c63eeec8eb
 };
 
 // when val == 0 nothing left over, so create character array from parameter pack
