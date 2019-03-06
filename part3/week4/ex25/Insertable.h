f5764e134fd89e954915f02be29a999809754b55
Remi <r.f.niel@student.rug.nl>
2019-03-06 21:10:30 +0100

    split up 25 and 26

---
+++ part3/week4/ex25/Insertable.h
@@ -0,0 +1,17 @@
+#ifndef INSERTABLE_
+#define INSERTABLE_
+
+using namespace std;
+
+template<template <typename> class Policy, typename Data>
+struct Insertable: public Policy<Data>
+{
+    Insertable() = default;
+    Insertable(Data in);
+    Insertable(Policy<Data>);
+
+    std::ostream &insertInto(std::ostream &out);
+};
+
+#undef HDR_
+#endif
