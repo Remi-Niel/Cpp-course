ac15a05979e5d1641bf54356f772975f2e692774
voldelord <r.f.niel@student.rug.nl>
2019-03-17 14:02:44 +0100

    ex42

--- part3/week4/ex34/main.cc
+++ part3/week4/ex34/main.cc
@@ -6,29 +6,39 @@
 // of course, to get another value specify that value instead of 25.
 // Alternatively, use this script to beautify the output:
 
+// #!/bin/bash
+// 
+// 
+// if [ $# -eq 0 ] ; then
+//     last=25
+// else
+//     last=$1
+// fi
+// 
+// if [ $# -gt 1 ] ; then
+//     g++ --std=c++0x -DLAST=${last} primefrank.cc | less
+// else
+//     g++ --std=c++0x -DLAST=${last} primefrank.cc |& 
+//         grep 'instantiation of' | sed 's/ instantiation of//'
+// fi
+
 
 #ifndef LAST
 #define LAST 18
 #endif
 
 enum 
-{ 
+{
+    IS_PRIME,
+    NO_PRIME,
+    CONTINUE  
 };
 
-// This struct is needed to determine whether you can stop sieving.
-//Determines 3 cases, if testvalue > sqrt(candidate) the candidate
-// IS_PRIME since it is not divisble by other numbers besides 1 and itself.
-// NO_PRIME if candidate is divisible by the testvalue. 
-// CONTINUE if it is not divisible by the current testvalue but might still be divisible,
-// by another value.
 template <int candidate, int testValue>
 struct Eval
 {
     enum
     { 
-        IS_PRIME,
-        NO_PRIME,
-        CONTINUE ,
         mode = 
             testValue * testValue > candidate ? IS_PRIME  :    
             candidate % testValue == 0 ?        NO_PRIME  :
@@ -36,11 +46,6 @@ struct Eval
     };
 };
 
-//Recursive definition, where candidate is the possible prime, and
-// is tested against the testvalue prime + 1. Sieve is then used again,
-// however mode is provided by Eval, in case of CONTINUE this default 
-// implementation of sieve is used again. However for mode = IS_PRIME and
-// mode = NO_PRIME specializations of sieve exist which exit the recursion.
 template <int candidate, int prime, int mode >
 struct sieve
 {
@@ -51,29 +56,24 @@ struct sieve
     };
 };
 
-//This sets isprime to IS_PRIME effectively exiting the recursion.
 template <int candidate, int prime> 
-struct sieve<candidate, prime, Eval<1, 1>::IS_PRIME>
+struct sieve<candidate, prime, IS_PRIME>
 {
     enum { isPrime = IS_PRIME };
 };
 
-//This sets isprime to NO_PRIME effectively exiting the recursion.
 template <int candidate, int prime>
-struct sieve<candidate, prime, Eval<1, 1>::NO_PRIME >
+struct sieve<candidate, prime, NO_PRIME >
 {
     enum { isPrime = NO_PRIME };
-}; 
+};
 
-//Initializes the sieve to start testing with testvalue = 2
 template <int prime>
 struct test
 {
     enum {isPrime = sieve<prime, 2, Eval<prime, 2>::mode>::isPrime };
 };
 
-//In the default case of the struct show, f() does not actually show
-// the prime. Instead it just continues to the next prime candidate.
 template <int prime, int isPrime>
 struct show
 {                           
@@ -83,10 +83,8 @@ struct show
     }
 };
 
-//If prime is a prime, the return statement causes compiler to print this
-// specific instantiation of static int* show<prime,0>::f()
 template <int prime>
-struct show<prime,  Eval<1, 1>::IS_PRIME>
+struct show<prime, IS_PRIME>
 {
     static int *f()
     {
@@ -97,16 +95,13 @@ struct show<prime,  Eval<1, 1>::IS_PRIME>
     }
 };
 
-//1 returns prime in test<> ::IsPrime and is the final case,
-// so here the program stops.
 template <>
-struct show<1,  Eval<1, 1>::IS_PRIME>
+struct show<1, IS_PRIME>
 {
     static void f()
     {}
 };
 
-//Starts the seach for primes at prime.
 template <int prime>
 void primes()
 {
