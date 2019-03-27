b3a17d037e2bbda4af3cce9d6b6d0207f15ef159
= <josvangoor@gmail.com>
2019-03-27 15:57:23 +0100

    ex55 met rule struct

--- part3/week7/ex55/parser/includes.h
+++ part3/week7/ex55/parser/includes.h
@@ -1,2 +1,16 @@
+#ifndef __INCLUDES_H
+#define __INCLUDES_H
+
 #include <string>
 #include <vector>
+
+
+struct Rule
+{
+    std::string lhs;
+    std::vector<std::string> rhs;
+};
+
+typedef std::vector<Rule> Rules;
+
+#endif
